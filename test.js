8b11ff098524ba95097f9bf4c079ab7f684bf662
TestGuy-1 <camoniz@students.wcpss.net>
2026-03-06 09:18:08 -0500

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
 function do1() {
-  console.log("ran")
-  alert("Button pressed")
+ conf1 = confirm("Do you want to download this?")
+  if (conf1) {
+console.log("Downloaded file")
+    do2()
+  } else {console.log("download Canceled")}
+}
+function do2() {
+  console.log("do2 success")
 }
